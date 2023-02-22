f5c673793de85cdfc72b005e57cb5fbd6e5a2f38
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-22 16:59:19 +0530

    added form input to change rooms

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
+const app = express();
 var router = express.Router();
+const bodyParser = require("body-parser");
+
+app.use(express.urlencoded({ extended: true }));
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +14,11 @@ router.get('/', function(req, res, next) {
 router.get( "/:title" , function (req , res ){
   res.render("index" , { title : req.params.title });
 })
+
+router.post('/' , (req , res) => {
+  console.log( req.body.roomName);
+  res.redirect( "/" + req.body.roomName) ;
+});
+
 
 module.exports = router;
