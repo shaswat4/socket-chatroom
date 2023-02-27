fb8752d62fee8c71a83848486392146836ba266a
shaswat4 <shaswatagrawal01@gmail.com>
2023-02-27 10:30:15 +0530

    made signin and register page with mongoDB

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,58 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const User = mongoose.model('user', { email : String , password : String  });
+
+
+router.get( '/signin' , function(req , res){
+  res.render('auth' , {title : "Sign In" , postSubmit : "signin"} );
+
+});
+
+router.post('/signin', function (req, res) {
+  const  email_ = req.body.email;
+  const password_ = req.body.password;
+  User.findOne({ email: email_ , password : password_ }, function (err, user) {
+    if (err) {
+      return res.status(500).json({ error: err });
+    }
+    if (!user) {
+      return res.status(401).json({ error: 'User email or password is incorrect' });
+    }
+    else {
+      res.status(200).json({ message: 'Login successful' });
+    }
+      
+    });
+  });
+
+
+
+
+
+
+
+router.get( "/register" , function(req , res){
+  res.render('auth' , {title : "Register" , postSubmit : "register" } );
+} )
+
+router.post( '/register' , (req , res)=> {
+
+  const temp = new User({ email : req.body.email , password : req.body.password });
+  temp.save().then(() => console.log('saved in db'));
+
+  res.redirect('/signin');
+
+
+
+});
+
+
+
 router.get( "/:title" , function (req , res ){
   res.render("index" , { title : req.params.title });
 })
@@ -19,6 +71,7 @@ router.post('/' , (req , res) => {
   console.log( req.body.roomName);
   res.redirect( "/" + req.body.roomName) ;
 });
+
 
 
 module.exports = router;
