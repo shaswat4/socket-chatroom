7f2908c93ad239ff0c2d4f15ad81d74c582d779c
shaswat4 <shaswatagrawal01@gmail.com>
2023-03-28 15:50:23 +0530

    defind seed:undo path

--- seeders/20230328064143-Group-attribute.js
+++ seeders/20230328064143-Group-attribute.js
@@ -3,16 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-
     await queryInterface.bulkInsert(
       "group_attributes",
       [
@@ -47,11 +37,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("group_attributes", null, {});
+   
   }
 };
