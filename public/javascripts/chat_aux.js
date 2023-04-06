5fe9fd9460e0b450c312a05e2bd79ec423ac8f92
shaswat4 <shaswatagrawal01@gmail.com>
2023-04-06 15:03:51 +0530

    added evt listener and chat state management

--- public/javascripts/chat_aux.js
+++ public/javascripts/chat_aux.js
@@ -1,3 +1,6 @@
+let current_chat = null;
+let current_chat_messages = null;
+
 function hello() {
   console.log("hello world");
 }
@@ -7,10 +10,10 @@ function p(params) {
 }
 
 /**
- * takes json in object and 
+ * takes json in object and
  * string [search, active] in option
  * passes data according to option
- * displayes search and active chat list in 
+ * displayes search and active chat list in
  * div class chat-nav-body
  */
 function displayList(object, option) {
@@ -32,6 +35,7 @@ function displayList(object, option) {
         } else {
           span.attr("data-chat-type", "user");
           span.attr("data-user-id", value.user_id);
+          span.attr("data-name", value.name);
         }
 
         span.appendTo(ul_obj);
@@ -108,26 +112,123 @@ function displayList(object, option) {
 }
 
 /**
- * handles clicks on active chat items 
+ * sets sends data in form of attributes
+ * to input tag
  */
-function clickOnActiveChatItem(e) {
-  let group_id = $(this).attr("data-group-id");
+function setMessageInputAtrributes(target, object) {
+  Object.keys(object).forEach((attr_name) => {
+    let name = _.kebabCase(attr_name);
+    let value = object[attr_name];
+    target.attr(name, value);
+  });
+}
 
+/**
+ *
+ * @param {*} target - current tag
+ * @returns attribute data object
+ */
+function getAttributesFromTag(target) {
+  let group_id = target.attr("data-group-id");
+  let type = target.attr("data-chat-type");
+  let name = target.attr("data-name");
+
+  let object = {
+    group_id: group_id,
+    type: type,
+    user_id: null,
+    name: name,
+  };
+
+  if (type == "user") {
+    let user_id = target.attr("data-user-id");
+    object["user_id"] = user_id;
+  }
+
+  return object;
+}
+
+function getMessageFromAPI(group_id) {
+  let temp = null;
   $.ajax({
     url: "chat/getMessage/new",
     type: "POST",
     data: { group_id: group_id },
     success: function (data) {
       // Handle the response from the server
+      // renderChatMain(data);
 
-      //renderChatMain(data);
+      current_chat_messages = data;
       renderMessages(data);
+
+      // let ele = $(".chat-main input#input");
+      // setMessageInputAtrributes(ele, object);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       // Handle errors
       console.error("Error: " + textStatus + " - " + errorThrown);
     },
   });
+
+  return temp;
+}
+
+/**
+ * handles clicks on active chat items
+ */
+function clickOnActiveChatItem(e) {
+  let target = $(this);
+  let object = getAttributesFromTag(target);
+  p(object);
+  current_chat = object;
+
+  getMessageFromAPI(current_chat.group_id);
+}
+
+async function clickOnsearchItem(e) {
+  let target = $(this);
+  let object = getAttributesFromTag(target);
+  current_chat = object;
+
+  //p(object)
+
+  if (current_chat.type == "user") {
+    await $.ajax({
+      url: "chat/searchGroupID",
+      type: "POST",
+      data: { user_id: current_chat.user_id },
+      success: function (data) {
+        // Handle the response from the server
+        //renderChatMain(data);
+
+        p(data);
+        current_chat.group_id = data.group_id;
+        p(current_chat);
+
+        let ele = $(".chat-main input#input");
+        setMessageInputAtrributes(ele, object);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // Handle errors
+        console.error("Error: " + textStatus + " - " + errorThrown);
+      },
+    }).catch((ele) => {});
+  }
+
+  p(current_chat);
+
+  if (current_chat.group_id != null) {
+    getMessageFromAPI(current_chat.group_id);
+  } else {
+    let temp = {
+      header: {
+        name: current_chat.name,
+      },
+      chats: [],
+    };
+
+    renderMessages(temp);
+  }
 }
 
 function renderMessages(data) {
