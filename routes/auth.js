5f9d6f4e1592e1873b8dd41fce22f18a93cdd8fb
shaswat4 <shaswatagrawal01@gmail.com>
2023-04-11 17:41:06 +0530

    added google auth with passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,96 +1,121 @@
-var express = require('express');
+var express = require("express");
 const app = express();
 var router = express.Router();
 const bodyParser = require("body-parser");
 // var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+var LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
 
-process.env.DEBUG = 'passport:*';
+process.env.DEBUG = "passport:*";
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var flash = require('connect-flash');
-const { ObjectID } = require('bson');
-const { isDataView } = require('util/types');
+var flash = require("connect-flash");
+const { ObjectID } = require("bson");
+const { isDataView } = require("util/types");
 app.use(flash());
-
 
 const { Sequelize, Op, Model, DataTypes } = require("sequelize");
 
-const db = require('../models'); 
+const db = require("../models");
 
 const sequelize = db.sequelize;
 
-const Users = db.Users ;
+const Users = db.Users;
 const Groups = db.Groups;
-const Group_User = db.Group_User ;
-const Chats = db.Chats ;
+const Group_User = db.Group_User;
+const Chats = db.Chats;
 
-
-const passport = require('./passport');
-
+const passport = require("./passport");
 
 function isSignedIn(req, res, next) {
-    try {
-      //no passport session
-      if (!req.session.passport) {
-        return res.redirect("/signin");
-      }
-    } catch (error) {
+  try {
+    //no passport session
+    if (!req.session.passport) {
       return res.redirect("/signin");
     }
-    next();
+  } catch (error) {
+    return res.redirect("/signin");
   }
-  
-  function p(params) {
-    console.log(params);
-  }
+  next();
+}
 
+function p(params) {
+  console.log(params);
+}
 
 /* main routes */
 
 router.get("/signin", function (req, res) {
-    res.render("auth", { title: "Sign In", postSubmit: "signin" , message : req.flash("info") });
+  res.render("auth", {
+    title: "Sign In",
+    postSubmit: "signin",
+    message: req.flash("info"),
   });
-  
-  router.post("/signin",
-    passport.authenticate("local", {
-      successRedirect: "/",
-      failureRedirect: "/signin",
-    })
-  );
-  
-  router.get("/register", function (req, res) {
-    res.render("auth", { title: "Register", postSubmit: "register" , message : req.flash("info") });
+});
+
+router.post(
+  "/signin",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/signin",
+  })
+);
+
+router.get("/register", function (req, res) {
+  res.render("auth", {
+    title: "Register",
+    postSubmit: "register",
+    message: req.flash("info"),
   });
-  
-  router.post("/register", async (req, res) => {
-  
-    let temp = await Users.findOne({where:{
-      username : req.body.username
-    }});
-  
-    if (temp){
-      req.flash("info", "This username already exists");
-      return res.redirect('/register');
-    }
-  
-    await Users.create({ 
+});
+
+router.post("/register", async (req, res) => {
+  let temp = await Users.findOne({
+    where: {
       username: req.body.username,
-      password: req.body.password
-    }).then(() => console.log("saved in db"));
-  
+    },
+  });
+
+  if (temp) {
+    req.flash("info", "This username already exists");
+    return res.redirect("/register");
+  }
+
+  await Users.create({
+    username: req.body.username,
+    password: req.body.password,
+  }).then(() => console.log("saved in db"));
+
+  res.redirect("/signin");
+});
+
+router.post("/logout", function (req, res, next) {
+  req.session.destroy(function (err) {
+    if (err) {
+      return next(err);
+    }
     res.redirect("/signin");
   });
-  
-  router.post("/logout", function (req, res, next) {
-    req.session.destroy(function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/signin");
-    });
-  });
-  
+});
+
+router.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: [
+      "https://www.googleapis.com/auth/userinfo.profile",
+      "https://www.googleapis.com/auth/userinfo.email",
+    ],
+  })
+);
+
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/signin" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
+
 module.exports = router;
--- routes/passport.js
+++ routes/passport.js
@@ -1,62 +1,108 @@
-var express = require('express');
+var express = require("express");
 const app = express();
 var router = express.Router();
 const bodyParser = require("body-parser");
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+require("dotenv").config();
 
-process.env.DEBUG = 'passport:*';
+process.env.DEBUG = "passport:*";
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-var flash = require('connect-flash');
-const { ObjectID } = require('bson');
-const { isDataView } = require('util/types');
+var flash = require("connect-flash");
+const { ObjectID } = require("bson");
+const { isDataView } = require("util/types");
 app.use(flash());
 
 const { Sequelize, Op, Model, DataTypes } = require("sequelize");
 
-const db = require('../models'); 
+const db = require("../models");
 
 const sequelize = db.sequelize;
 
-const Users = db.Users ;
-
-
+const Users = db.Users;
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
-passport.use( 'local' , new LocalStrategy(
-  async function(username, password, done) {
+passport.use(
+  "local",
+  new LocalStrategy(async function (text, password, done) {
+    // console.log('\n\n\n')
+    // console.log(text)
+    // console.log(passport)
     Users.findOne({
-      where : { 
-        username : username ,  
-        password : password
-      }
-    }).then( user => {
-      
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username or password.' });
-      }
-      // if (user.password !== password) {
-      //   return done(null, false, { message: 'Incorrect password.' });
-      // }
-      return done(null, user);
+      where: {
+        [Op.or]: [
+          {
+            username: text,
+          },
+          {
+            email: text,
+          },
+        ],
+        password: password,
+      },
+    })
+      .then((user) => {
+        if (!user) {
+          return done(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+        // if (user.password !== password) {
+        //   return done(null, false, { message: 'Incorrect password.' });
+        // }
+        // console.log(user)
+        return done(null, user);
+      })
+      .catch((err) => done(err));
+  })
+);
 
-    }).catch(err => done(err));
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
 
-  }
-));
+      let user = await Users.findOne({
+        where: {
+          email: profile.emails[0].value,
+        },
+      });
+
+      if ( user != null  ){
+        console.log(user)
+        return cb(null , user);
+      }
+
+      cb(null);
+      // Users.findOrCreate({ googleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+    }
+  )
+);
 
 passport.serializeUser(function (user, done) {
   process.nextTick(function () {
-    done(null, { id: user.user_id, username: user.username });
+    done(null, {
+      id: user.user_id,
+      username: user.username,
+      email: user.email,
+    });
   });
 });
-
 
 passport.deserializeUser(function (user, done) {
   process.nextTick(function () {
@@ -64,4 +110,4 @@ passport.deserializeUser(function (user, done) {
   });
 });
 
-module.exports =passport; 
+module.exports = passport;
