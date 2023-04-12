7cf4d30021c3109bd09e418524ce416eb0b61a7f
shaswat4 <shaswatagrawal01@gmail.com>
2023-04-12 14:48:26 +0530

    added email to register page and some minor changes

--- routes/auth.js
+++ routes/auth.js
@@ -47,7 +47,7 @@ function p(params) {
 /* main routes */
 
 router.get("/signin", function (req, res) {
-  res.render("auth", {
+  res.render("signin", {
     title: "Sign In",
     postSubmit: "signin",
     message: req.flash("info"),
@@ -57,35 +57,69 @@ router.get("/signin", function (req, res) {
 router.post(
   "/signin",
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/chat",
     failureRedirect: "/signin",
   })
 );
 
 router.get("/register", function (req, res) {
-  res.render("auth", {
+  res.render("register", {
     title: "Register",
     postSubmit: "register",
     message: req.flash("info"),
   });
 });
 
+/**
+ * registers users
+ * takes email , username , password
+ * registers if email isn't present in db
+ * and if username dosen't exist
+ *
+ */
 router.post("/register", async (req, res) => {
+  let email = req.body.email;
+  let username = req.body.username;
+  let password = req.body.password;
+
   let temp = await Users.findOne({
     where: {
-      username: req.body.username,
+      [Op.or]: [
+        {
+          username: username,
+        },
+        {
+          email: email,
+        },
+      ],
     },
   });
 
-  if (temp) {
-    req.flash("info", "This username already exists");
-    return res.redirect("/register");
+  //record exists
+  // && !(temp.type.includes("l"))
+  if (temp != null) {
+    // if username exists
+    if (temp.username != null && temp.username == username) {
+      req.flash(
+        "info",
+        "This username already exists please choose another one"
+      );
+      return res.redirect("/register");
+    }
+
+    // if email exists
+    if (temp.email != null && temp.email.length > 0 && temp.email == email) {
+      req.flash("info", "This email is already registered!");
+      return res.redirect("/register");
+    }
   }
 
   await Users.create({
-    username: req.body.username,
-    password: req.body.password,
-  }).then(() => console.log("saved in db"));
+    username: username,
+    password: password,
+    email: email,
+    type: "l",
+  }).then(() => console.log("user saved in db"));
 
   res.redirect("/signin");
 });
@@ -114,7 +148,7 @@ router.get(
   passport.authenticate("google", { failureRedirect: "/signin" }),
   function (req, res) {
     // Successful authentication, redirect home.
-    res.redirect("/");
+    res.redirect("/chat");
   }
 );
 
